d913c35ec7d493113f00b23e60234ac65fc9ac40
Valeria Durruty <104698500+ValeriaDurruty@users.noreply.github.com>
2025-04-08 00:07:24 -0300

    Update streamlit_app.py
    fix line where API is

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,12 +37,13 @@ if ingredients_list:
     #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
 
-#To display info from smoothiefroot
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+
+
+#To display info from smoothiefroot
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
