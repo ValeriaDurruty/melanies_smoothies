bceca201dfd2ce252e98314d4aafc929a48600c8
Valeria Durruty <104698500+ValeriaDurruty@users.noreply.github.com>
2025-04-08 00:10:31 -0300

    Update streamlit_app.py
    fix to see json response

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ if ingredients_list:
 #To display info from smoothiefroot
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
